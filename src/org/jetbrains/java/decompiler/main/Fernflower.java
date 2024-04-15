72c07a2138bd58211e2c6057e161dfb752ae478c
Bartek Pacia <barpac02@gmail.com>
2024-04-15 22:18:02 +0100

    make TestSwitchWithDeconstructionsWithoutNestedJavac pass

--- src/org/jetbrains/java/decompiler/main/Fernflower.java
+++ src/org/jetbrains/java/decompiler/main/Fernflower.java
@@ -42,6 +42,10 @@ public class Fernflower implements IDecompiledData {
       catch (IllegalArgumentException ignore) { }
     }
 
+    for (final var property : properties.entrySet()) {
+      System.out.println(property.getKey() + "=" + property.getValue() + " \\");
+    }
+
     structContext = new StructContext(saver, this, new LazyLoader(provider));
     classProcessor = new ClassesProcessor(structContext);
 
